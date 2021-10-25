582be70ed3fcb4d3cfd00a329c1e161b0b4e386e
Emanuele Albertosi <macintosh@iMac.homenet.telecomitalia.it>
2021-10-25 15:15:51 +0200

    create message + decorator

--- monolith/views/messages.py
+++ monolith/views/messages.py
@@ -8,10 +8,27 @@ from monolith.forms import UserForm,UserDelForm,MessageForm
 messages = Blueprint('messages', __name__)
 
 
-@messages.route('/messages')
-def _messages():
-    _messages = db.session.query(Message)
-    return render_template("messages.html", messages=_messages)
+@messages.route('/mailbox')
+
+@login_required
+
+def mailbox():
+    # Retrieve user <id>
+    id = current_user.get_id()
+    # Retrieve sent/received messages of user <id>
+    _messages = db.session.query(Message).filter(
+        Message.sender_id == id or Message.recipient_id == id
+    )
+    return render_template("mailbox.html", messages=_messages)
+
+@messages.route('/message/<int:id>')
+@login_required
+def message(id):
+    _message = db.session.query(Message).filter(Message.id == id).first()
+    if _message is None:
+        return {'msg': 'message not found'}, 404
+    return render_template('message.html', message=_message)
+
 
 @messages.route('/write_message', methods=['POST','GET'])
 def write_message():
