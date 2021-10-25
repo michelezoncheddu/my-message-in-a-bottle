3228fa1f988b5d6625eeb300fd93adf659215e15
laura <laura.norato.1994@gmail.com>
2021-10-25 20:22:03 +0200

    added draft and send message buttons + skeleton for message draft query

--- monolith/views/messages.py
+++ monolith/views/messages.py
@@ -6,6 +6,8 @@ from ..auth import login_required
 from monolith.database import Message, db
 from monolith.forms import MessageForm
 
+from sqlalchemy.sql.expression import false
+
 messages = Blueprint('messages', __name__)
 
 
@@ -43,30 +45,41 @@ def message(id):
     return {'msg': 'message deleted'}, 200
 
 
-@messages.route('/write_message', methods=['POST', 'GET'])
+@messages.route('/create_message', methods=['POST','GET'])
 #@login_required
-def write_message():
+def create_message():
     form = MessageForm()
     if request.method == 'POST':
         #for recipient in form.recipient_id.data:
-            new_message=Message()
-            new_message.recipient_id=form.recipient_id.data
-            new_message.text=form.text_area.data
-            new_message.delivery_date= form.delivery_date.data
-            new_message.attachment=None
-            new_message.is_draft=True
-            new_message.is_delivered=False
-            new_message.is_valid=True
-            new_message.sender_id=form.sender_id.data
-            db.session.add(new_message) 
-            db.session.commit()          
-            return redirect('/messages')
+        if request.form['submit_button'] == 'Draft':
+            is_draft = True
+        else:
+            is_draft = False
+
+        new_message=Message()
+        new_message.recipient_id=form.recipient_id.data
+        new_message.text=form.text_area.data
+        new_message.delivery_date= form.delivery_date.data
+        new_message.attachment=None
+        new_message.is_draft= is_draft
+        new_message.is_delivered=False
+        new_message.is_valid=True
+        new_message.sender_id=form.sender_id.data
+        db.session.add(new_message) 
+        db.session.commit()          
+        return redirect('/messages')
 
     elif request.method == 'GET':
         return render_template("create_message.html", form=form) 
     else:
         raise RuntimeError('This should not happen!')   
     
+@messages.route('/messages/draft')
+def messages_draft():
+    messages_draft = db.session.query(Message)
+    for i in messages_draft.filter_by(is_draft = True):
+        print(i)
+    return render_template("messages.html", messages=messages_draft)
 
 @messages.route('/messages')
 def _messages():
