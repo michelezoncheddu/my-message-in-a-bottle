7892194ac97f2eab761776c3e50ce088afb3fc76
Matteo Pinna <71351275+NennoMP@users.noreply.github.com>
2021-10-27 01:21:45 +0200

    generalized save_image

--- monolith/image.py
+++ monolith/image.py
@@ -1,14 +1,9 @@
 import os
 from werkzeug.utils import secure_filename
 
-#helper function to save images to local directory from create_message form
-def save_image(file):
-    filename = secure_filename(file.filename)
-    file.save(os.path.join('monolith/images', filename))
-    return filename
-
-#utility function to save profile pictures
-def save_profile_picture(file):
+#utility function for saving attachments (messages) or profile pics (users)
+def save_image(file, path):
     filename = secure_filename(file.filename)
     file.save(os.path.join('monolith/static/profile', filename))
     return filename
+
