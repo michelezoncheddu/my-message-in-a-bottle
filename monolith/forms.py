fdb060076ea67c9aefb64114caf6ea3e2c30e823
michelezoncheddu <michelezoncheddu@users.noreply.github.com>
2021-10-27 17:14:25 +0200

    Add calendar for date form

--- monolith/forms.py
+++ monolith/forms.py
@@ -1,9 +1,9 @@
 import wtforms as f
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired,Regexp
-from wtforms import DateField, Form
+from wtforms import Form
+from wtforms.validators import DataRequired
+from wtforms.fields.html5 import DateField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-
 
 
 class LoginForm(FlaskForm):
@@ -17,7 +17,7 @@ class UserForm(FlaskForm):
     firstname = f.StringField('firstname', validators=[DataRequired()])
     lastname = f.StringField('lastname', validators=[DataRequired()])
     password = f.PasswordField('password', validators=[DataRequired()])
-    dateofbirth = f.DateField('dateofbirth', format='%d/%m/%Y')
+    dateofbirth = DateField('dateofbirth', format='%d/%m/%Y')
     display = ['email', 'firstname', 'lastname', 'password', 'dateofbirth']
 
 class UserDelForm(FlaskForm):
@@ -27,7 +27,7 @@ class UserDelForm(FlaskForm):
 class MessageForm(FlaskForm):
     text_area = f.TextAreaField('Insert message text')
     #sender_id = f.IntegerField('Sender Id', validators=[DataRequired()])
-    delivery_date = f.DateField('Delivery Date', format='%d/%m/%Y')
+    delivery_date = DateField('Delivery Date', format='%d/%m/%Y')
     image_file = FileField('Image',validators=[FileAllowed(['jpg','png'])]) 
     display = ['text_area', 'delivery_date','image_file']    
 
