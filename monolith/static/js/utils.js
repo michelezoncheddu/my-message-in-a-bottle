8708eb11b4e3eb88eae1beef9ae3ed8edecb1a15
NennoMP <matteo.pinna@hotmail.com>
2021-11-01 09:54:42 +0100

    moved toggleEdit in utils.js

--- monolith/static/js/utils.js
+++ monolith/static/js/utils.js
@@ -6,6 +6,10 @@ function DeleteMessage(id) {
     xhttp.send()
 }
 
-function test2() {
-  document.getElementById('test').removeAttribute('readonly');
+// Enable edit on readonly text
+function toggleEdit() {
+  document.getElementById('firstname').readOnly = false
+  document.getElementById('lastname').readOnly = false
+  document.getElementById('email').readOnly = false
+  document.getElementById('location').readOnly = false
 }
