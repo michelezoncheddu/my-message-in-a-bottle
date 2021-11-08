1aaa2c08faf687f7fac67411868a4d84d857b52b
michelezoncheddu <michelezoncheddu@users.noreply.github.com>
2021-11-08 11:58:44 +0100

    Fix delete message bug

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9
+FROM python:3.9-slim-buster
 ADD . /code
 WORKDIR /code
 #VOLUME /code
--- monolith/static/js/utils.js
+++ monolith/static/js/utils.js
@@ -1,16 +1,15 @@
 
 // Send delete request message <id>
 function DeleteMessage(id) {
-    var xhttp = new XMLHttpRequest()
-    xhttp.open("DELETE", "/message/" + id, true)
-    xhttp.send()
-    window.location.href = "/mailbox"
+    fetch('/message/' + id, {
+        method: 'DELETE'
+    }).then(_ => window.location.href = '/mailbox')
 }
 
 // Enable edit on readonly text
 function toggleEdit() {
-  document.getElementById('firstname').readOnly = false
-  document.getElementById('lastname').readOnly = false
-  document.getElementById('email').readOnly = false
-  document.getElementById('location').readOnly = false
+    document.getElementById('firstname').readOnly = false
+    document.getElementById('lastname').readOnly = false
+    document.getElementById('email').readOnly = false
+    document.getElementById('location').readOnly = false
 }
